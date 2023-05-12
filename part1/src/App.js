113a48bea2689ade1a42483a9bd86b61d30cd29e
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-05-12 13:52:38 +0300

    Display statistics in a component of its own

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const Header = (props) => (
      <>
         <h1>{props.course}</h1>
@@ -45,7 +47,45 @@ const Total = (props) => (
   </>
 )
 
+
+
+const Statistics = (props) => {
+let total = props.good+props.bad+props.neutral
+let average = total / 3
+let positive = props.good/total*100
+ return   (
+       <>
+           <h2>Statistics</h2>
+            <StatisticLine text="good" value ={props.good} />
+            <StatisticLine text="neutral" value ={props.neutral} />
+            <StatisticLine text="bad" value ={props.bad} />
+            <StatisticLine text="all" value ={total} />
+            <StatisticLine text="average" value ={average} />
+            <StatisticLine text="positive" value ={positive} />
+      </>
+     )
+}
+
+const StatisticLine = (props) => (
+  <>
+    <p>{props.text}  {props.value}</p>
+  </>
+)
+
+const Button = (props) => (
+  <div style={{display:"flex"}}>
+   <button onClick={props.handleGood} style={{margin:"8px"}}>Good</button>
+    <button onClick={props.handleNuetral} style={{margin:"8px"}}>Neutral</button>
+    <button onClick={props.handleBad} style={{margin:"8px"}}>Bad</button>
+  </div>
+  
+)
+
 const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   const course = {
     name: 'Half Stack application development',
     parts: [
@@ -64,11 +104,30 @@ const App = () => {
     ]
   }
 
+  const handleGood = () => {
+    setGood(good+1)
+  }
+
+  const handleBad = () => {
+    setBad(bad+1)
+  }
+
+  const handleNuetral = () => {
+    setNeutral(neutral+1)
+  }
+
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
+    <h2>Give feedback</h2>
+    <Button handleBad={handleBad} handleNuetral={handleNuetral} handleGood={handleGood}/>
+     {
+       (good>=1 &&  <Statistics good={good} bad={bad} neutral={neutral}/>) 
+     || (bad>=1 &&  <Statistics good={good} bad={bad} neutral={neutral}/>)
+     || (neutral>=1 &&  <Statistics good={good} bad={bad} neutral={neutral}/>)
+     } 
     </div>
   )
 }
